7950aec9d9bef76b033078dd2218fc6cfd120fdf
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2024-11-14 12:15:50 +0100

    .

--- re.sh
+++ re.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 
 file="main.tex"
 
+pdflatex $file
 mupdf main.pdf &
-echo $file | entr -cn sh -c "pdflatex $file; pkill -HUP mupdf"
+echo $file | entr -cn sh -c "clear; pdflatex $file; pkill -HUP mupdf"
